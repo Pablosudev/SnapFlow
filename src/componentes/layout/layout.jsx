a9cde12e6c4cf7c2571d74a574d83de36bbf0af2
Pablosudev <pablo.losu.dev@gmail.com>
2024-11-19 09:58:03 +0100

    first commit

---
+++ src/componentes/layout/layout.jsx
@@ -0,0 +1,36 @@
+import { NavLink, Outlet } from "react-router-dom"
+import "./navbar.css"
+import "./header.css"
+import "./footer.css"
+import { CiSearch } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
+import { FaXTwitter } from "react-icons/fa6";
+
+export const Layout = () => {
+
+
+    return <>
+        <nav className="navBar">
+            <h1 className="navBar__title">SNAPFLOW</h1>
+            <ul className="navBar__link">
+               <NavLink className="navBar__page">HOME</NavLink>
+               <NavLink className="navBar__page">MY PHOTOS</NavLink>
+            </ul>
+        </nav>   
+        <header>
+            <img className="header__image" src="/img/pexels-photo-1655166.jpeg" alt="Mar_foto" />
+            <h2 className="header__title">WELCOME TO YOUR <br /> IMAGE COLLECTION</h2>
+            <p className="header__text">Explore, save, and organize your favorite <br/> images all in one place</p>
+            <input className="header__search" type="text" placeholder="Search Image" />
+            <button className="header__button"><CiSearch/></button>
+        </header>
+        <Outlet/> 
+        <footer className="footer">
+            <h1 className="footer__title">SNAPFLOW</h1>
+            <CiLinkedin className="footer__linkedin"/>
+            <FaInstagram className="footer__instagram"/>
+            <FaXTwitter className="footer__x"/>
+        </footer>
+    </>
+}
---
+++ src/pages/homePage.jsx
@@ -0,0 +1,13 @@
+
+
+
+export const HomePage = () => {
+
+
+
+    return <>
+        <header>
+            <h1>HOLAM MUNDO</h1>
+        </header>
+    </>
+}
