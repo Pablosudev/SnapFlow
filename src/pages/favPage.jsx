5871eae1f1f0b9ac711cff658d906aa509a2abbb
Pablosudev <pablo.losu.dev@gmail.com>
2024-12-09 18:45:41 +0100

    Pag Fav solucionado

--- src/pages/favPage.jsx
+++ src/pages/favPage.jsx
@@ -1,11 +1,25 @@
-import { ImagesComponent } from "../components/imgComponentes/imagesComponent"
+import { RiDownload2Line } from "react-icons/ri";
+import { FaEdit } from "react-icons/fa";
+import { RiDeleteBin6Line } from "react-icons/ri";
+
 
 export const FavPage = () => {
 
     const photosFav = localStorage.getItem("fav")
     const imagesFav = JSON.parse(photosFav)
-    console.log(imagesFav)
+    
+    
+
     return <>
-    <ImagesComponent data={data} type="images"/>
+    {imagesFav.map((images,index) => {
+    return <div key={index} className="images">
+        <img src={images.urls.small} className="images__random"/>
+        <div className="buttonsImages">
+            <button onClick={() => handleDownload(images)} className="buttonFavs"><RiDownload2Line /></button>
+            <button onClick={() => handleEdit(images)} className="buttonFavs"><FaEdit /></button>
+            <button onClick={() => handleDelete(images)} className="buttonFavs"><RiDeleteBin6Line /></button>
+        </div>
+    </div>
+})}
     </>
 }
