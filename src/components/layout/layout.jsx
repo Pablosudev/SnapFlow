754a726f4b63df39a7f3123d886c6231fd22006e
Pablosudev <pablo.losu.dev@gmail.com>
2025-01-30 14:56:00 +0100

    header change

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,69 +1,38 @@
-import { NavLink, Outlet, useLocation } from "react-router-dom"
-import "./navbar.css"
-import "./header.css"
-import "./footer.css"
-import { CiSearch } from "react-icons/ci";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
+import "./navbar.css";
+import "./header.css";
+import "./footer.css";
 import { FaInstagram } from "react-icons/fa";
 import { CiLinkedin } from "react-icons/ci";
 import { FaXTwitter } from "react-icons/fa6";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { searchImagesThunk } from "../../features/images/imagesThunk";
-import { searchFavThunk } from "../../features/images/favThunk";
+
+
 
 export const Layout = () => {
-    const [searchTerm,setSearchTerm] = useState('')
-    const dispatch = useDispatch();
-    const location = useLocation();
+  
 
-    const handleSearch = (e) => {
-        e.preventDefault();
-
-        if (searchTerm.trim()) {
-            
-            if (location.pathname === '/'){
-                dispatch(searchImagesThunk(searchTerm));
-            } else if (location.pathname === '/fav') {
-                dispatch(searchFavThunk(searchTerm))
-            }
-        }
-    };
-
-    
-    
-
-    return <>
-        <div className="page">
+  return (
+    <>
+      <div className="page">
         <nav className="navBar">
-            <h1 className="navBar__title">SNAPFLOW</h1>
-            <ul className="navBar__link">
-               <NavLink to="/" className="navBar__home">HOME</NavLink>
-               <NavLink to="/fav" className="navBar__fav">MY PHOTOS</NavLink>
-            </ul>
-        </nav>   
-        <header className="header">
-            <img className="header__image" src="/img/pexels-photo-1655166.jpeg" alt="Mar_foto" />
-            <h2 className="header__title">WELCOME TO YOUR <br /> IMAGE COLLECTION</h2>
-            <p className="header__text">Explore, save, and organize your favorite <br/> images all in one place</p>
-            <form onSubmit={handleSearch}>
-                    <input
-                        className="header__search"
-                        type="text"
-                        placeholder="Find your perfect image"
-                        value={searchTerm}
-                        onChange={(e) => {
-                            setSearchTerm(e.target.value)}}
-                    />
-            <button  className="header__button"><CiSearch/></button>
-            </form>
-        </header>
-        <Outlet/> 
+          <h1 className="navBar__title">SNAPFLOW</h1>
+          <ul className="navBar__link">
+            <NavLink to="/" className="navBar__home">
+              HOME
+            </NavLink>
+            <NavLink to="/fav" className="navBar__fav">
+              MY PHOTOS
+            </NavLink>
+          </ul>
+        </nav>
+        <Outlet />
         <footer className="footer">
-            <h1 className="footer__title">SNAPFLOW</h1>
-            <CiLinkedin className="footer__linkedin"/>
-            <FaInstagram className="footer__instagram"/>
-            <FaXTwitter className="footer__x"/>
+          <h1 className="footer__title">SNAPFLOW</h1>
+          <CiLinkedin className="footer__linkedin" />
+          <FaInstagram className="footer__instagram" />
+          <FaXTwitter className="footer__x" />
         </footer>
-        </div>
+      </div>
     </>
-}
+  );
+};
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,40 +1,76 @@
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { getImagesData, getImagesStatus } from "../features/images/imagesSlice"
-import { imagesThunk } from "../features/images/imagesThunk"
-import { ImagesComponent } from "../components/imgComponentes/imagesComponent"
-
-
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getImagesData, getImagesStatus } from "../features/images/imagesSlice";
+import { imagesThunk } from "../features/images/imagesThunk";
+import { ImagesComponent } from "../components/imgComponentes/imagesComponent";
+import { searchImagesThunk } from "../features/images/imagesThunk";
+import { CiSearch } from "react-icons/ci";
+import { useLocation } from "react-router-dom";
 export const HomePage = () => {
+  const [data, setData] = useState([]);
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true);
 
-   const [data,setData] = useState([])
-   const dispatch = useDispatch()
-   const [isLoading,setIsLoading] = useState(true)
-   
+  const imagesData = useSelector(getImagesData);
+  const imagesStatus = useSelector(getImagesStatus);
+  const [searchTerm, setSearchTerm] = useState("");
+  const location = useLocation();
 
-   const imagesData = useSelector(getImagesData)
-   const imagesStatus = useSelector(getImagesStatus)
-   
-   
+  useEffect(() => {
+    if (imagesStatus === "idle") {
+      dispatch(imagesThunk());
+    } else if (imagesStatus === "fulfilled") {
+      setData(imagesData);
+      setIsLoading(false);
+    } else if (imagesStatus === "rejected") {
+      setIsLoading(false);
+      alert("Error");
+    }
+  }, [imagesData, imagesStatus, dispatch]);
 
-    useEffect(() => {
-         if(imagesStatus === "idle" ){
-            dispatch(imagesThunk())
-         }
-         else if (imagesStatus === "fulfilled"){
-            setData(imagesData)
-            setIsLoading(false)
-            
-         }
-         else if (imagesStatus === "rejected"){
-            setIsLoading(false)
-            alert ("Error")
-         }
-      },[imagesData,imagesStatus,dispatch])
+  const handleSearch = (e) => {
+    e.preventDefault();
 
-    return <>
-    <div >
-    <ImagesComponent data={data} type="images"/>
-    </div>
+    if (searchTerm.trim()) {
+      if (location.pathname === "/") {
+        dispatch(searchImagesThunk(searchTerm));
+      }
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <header className="header">
+          <img
+            className="header__image"
+            src="/img/pexels-photo-1655166.jpeg"
+            alt="Mar_foto"
+          />
+          <h2 className="header__title">
+            WELCOME TO YOUR <br /> IMAGE COLLECTION
+          </h2>
+          <p className="header__text">
+            Explore, save, and organize your favorite <br /> images all in one
+            place
+          </p>
+          <form onSubmit={handleSearch}>
+            <input
+              className="header__search"
+              type="text"
+              placeholder="Find your perfect image"
+              value={searchTerm}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+            />
+            <button className="header__button">
+              <CiSearch />
+            </button>
+          </form>
+        </header>
+        <ImagesComponent data={data} type="images" />
+      </div>
     </>
-}
+  );
+};
